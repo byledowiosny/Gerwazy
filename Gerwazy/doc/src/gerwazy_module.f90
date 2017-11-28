4ab20c0e3222618551a1f58aa8cea89fdcee13f9
Georges Tomaka <georges@tomaka.org>
2017-11-28 17:53:51 +0000

    Moving up from fortran to c++

--- Gerwazy/doc/src/gerwazy_module.f90
+++
@@ -1,9 +0,0 @@
-module gerwazy_module
-
-  use chdir_module
-  use welcome_module
-  use readline_module
-  use qsort_module
-  use mode_module
-
-end module gerwazy_module
--- Gerwazy/src/gerwazy_module.f90
+++
@@ -1,9 +0,0 @@
-module gerwazy_module
-
-  use chdir_module
-  use welcome_module
-  use readline_module
-  use parse_module
-  use ticks_module
-
-end module gerwazy_module
